0ccea63be47585702ea3a2393bf81696b4062c6a
Marius Odediran <themariusodediran@gmail.com>
2025-01-26 11:31:52 +0100

    Changes made

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif'],
+      JetBrainsMono: ['JetBrains Mono', 'monospace'],
+      Lobster: ['Lobster', 'cursive']
+    }
   },
   plugins: [],
 }
-
