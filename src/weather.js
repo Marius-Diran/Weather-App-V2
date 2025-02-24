ef0564e16b3f7c28d351ca96c80614dcd620dcec
Marius Odediran <themariusodediran@gmail.com>
2025-02-24 21:49:00 +0100

    Making progress

--- src/weather.js
+++ src/weather.js
@@ -2,54 +2,69 @@ const APIkey = 'a85989d2ccc1b494920a4a9758266b73';
 const searchBtn = document.querySelector('.search-btn');
 const locationInput = document.querySelector('.search-input');
 const description = document.querySelector('.description');
-const temp = document.querySelector('.temp');
+const temps = document.querySelector('.temp');
 const weatherCards = document.querySelector('.weather-cards');
 const cityChange = document.querySelector('.cities');
-const city = locationInput.value.trim();
+
 
 searchBtn.addEventListener('click', () => {
-  if (locationInput) {
-    getWeather(locationInput)
+  getWeather()
+  if (locationInput.value.trim().length > 0) {
+    cityChange.innerHTML = locationInput.value;
   }
 });
 
 locationInput.addEventListener('keyup', (e)=> {
   if (e.key === 'Enter') {
+    getWeather()
     if (locationInput.value.trim().length > 0) {
       cityChange.innerHTML = e.target.value;
     }
   }
 })
 
-function getWeather(city) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`)
+function getWeather() {
+  const city = locationInput.value.trim()
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`;
+
+  fetch(url)
     .then((response) => response.json()).then(json => {
-      switch (json.weather[0].main){
-        case 'clear':
+
+      const image = document.querySelector('.weather-icon');
+
+      switch (json.weather[0].main) {
+        case 'Clear':
           image.src = 'assets/sunlight.png';
           break;
           
-        case 'cloudy':
+        case 'Clouds':
           image.src = 'assets/cloudy.png';
           break;
 
-        case 'rain':
-          Image.src = 'assets/rain.png';
+        case 'Rain':
+          image.src = 'assets/rain.png';
           break;
         
-        case 'windy':
+        case 'Snow':
           image.src = 'assets/windy.png';
           break;
 
-        case 'windy-rain':
+        case 'Drizzle':
           image.src = 'assets/windy-rain.png';
           break;
 
-        case 'cloudy-cloud-rain':
+        case 'Thunderstorm':
           image.src = 'assets/cloudy-cloud-rain.png'
+          break;
+
+        default:
+          image.src = 'assets/sunlight.png';
           break;
       }
 
-      temp.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      temps.innerHTML = `${Math.round(json.main.temp)}<span>°C</span>`;
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
 }
